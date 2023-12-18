16b3fca4957ad947682d1b670ac17313af48459c
Dmitriy Ignatenko <titarx@gmail.com>
2023-12-18 11:36:17 +0400

    Fix to check if the path exists. Write to a file.

--- main.pl
+++ main.pl
@@ -22,7 +22,11 @@ while (my $line = <$log_file>) {
     if (my @path_search_result = $line =~ m/\d]\s"[A-Z]{3,7}\s(\/[^\s]+?)\s/is) {
         if ($path_search_result[0]) {
             my $path_search = "@{[$root_path]}@{[$path_search_result[0]]}";
-            if (-f $path_search && !grep(/^$path_search_result[0]$/, @result_list)) {
+
+#             print $path_search;
+#             print "\n";
+
+            if (-e $path_search && !grep(/^$path_search_result[0]$/, @result_list)) {
                 push @result_list, $path_search_result[0];
             }
         }
@@ -32,12 +36,14 @@ close $log_file;
 
 @result_list = sort @result_list;
 
-print join("\n", @result_list);
-print "\n";
+# print join("\n", @result_list);
+# print "\n";
 
-# my $result_file_path = "@{[$current_folder_path]}/result.txt";
-# open(my $result_file, '>', $result_file_path) or exit 'Cannot create or open result file';
-# # print $result_file 'Проверка';
-# close $result_file;
+my $result_file_path = "@{[$current_folder_path]}/result.txt";
+open(my $result_file, '>', $result_file_path) or exit 'Cannot create or open result file';
+# print $result_file 'Проверка';
+print $result_file join("\n", @result_list);
+print $result_file "\n";
+close $result_file;
 
 print 'Done';
